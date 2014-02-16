43d561f2f993e4ce10bbf5932ba44b6f235c054d
Jeffrey Paul <sneak@datavibe.net>
2014-02-17 00:46:14 +0100

    refactored script, created setuptools config

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='sqlite2json',
+    version='1.0.0',
+    author='John gerlock',
+    author_email='john@pikkey.com',
+    packages=['sqlite2json'],
+    url='https://github.com/john-g-g/sqlite2json',
+    license=open('LICENSE').read(),
+    description='SQLite to JSON converter',
+    long_description=open('README.md').read(),
+    entry_points = {
+        'console_scripts': [
+            'sqlite2json = sqlite2json:main'
+        ]
+    }
+)
